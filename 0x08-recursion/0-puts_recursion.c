4317388c5111bf511724142299dac49fe5ef22f9
Dani-dzoe <danieldzotepe568@gmail.com>
2023-01-05 01:53:57 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - this is a func to print string recursively
+ * @s: parameter string
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursions(s);
+}
