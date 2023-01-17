657d4211c2b063f1d65e01fdee6c60096698ff72
Dani-dzoe <danieldzotepe568@gmail.com>
2023-01-17 04:52:18 -0800

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- * Description: Longer description
+ * Description: a very nice looking dog
  */
 struct dog
 {
@@ -15,9 +15,6 @@ struct dog
 	char *owner;
 }
 
-/*dog_t - typedef for struct dog*/
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
