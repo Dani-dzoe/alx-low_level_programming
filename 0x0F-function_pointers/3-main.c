ee31c24f80574b7a7bdeaa2b7491b80f51c4f1c5
Dani-dzoe <danieldzotepe568@gmail.com>
2023-01-19 11:54:19 -0800

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: number of argu
+ * @argv: array of argu
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	o = *argc[2];
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	result = func(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
