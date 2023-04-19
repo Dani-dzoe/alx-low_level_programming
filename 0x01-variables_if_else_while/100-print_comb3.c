f1685eb6872df1c14d581d2ed6041ce4b213326f
Dani-dzoe <danieldzotepe568@gmail.com>
2023-04-19 13:38:24 -0700

    quick fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,18 @@
  */
 int main(void)
 {
-	int i;
-	
-	for (i = 0; i < 100; i++);
-	{
-		putchar((i / 10) + '0');
-		purchar((i % 10) + '0');
-	}
-	putchar('\n');
+	int i, j;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
 	return (0);
 }
