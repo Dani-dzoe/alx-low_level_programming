b473462bc84ad2332fa9203a48628fa42af650d8
Dani-dzoe <danieldzotepe568@gmail.com>
2023-04-19 15:56:36 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+	return (0);
+}	
