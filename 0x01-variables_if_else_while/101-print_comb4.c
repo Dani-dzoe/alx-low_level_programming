9ad05345cbe642f5447b577024ed23fe65c42187
Dani-dzoe <danieldzotepe568@gmail.com>
2023-04-19 13:45:51 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all posssible combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+	return (0);
+}
