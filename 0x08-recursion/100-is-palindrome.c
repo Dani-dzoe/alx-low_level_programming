1fa1187c3c36eb6d349b6c8b340a95d48387458a
Dani-dzoe <danieldzotepe568@gmail.com>
2025-07-23 20:35:59 +0000

    added main files and added advanced tasks

---
+++ 0x08-recursion/100-is-palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function to return a palindrome
+ * Return: 1 if string is palindrome, else 0.
+ */
+int is_palindrome(char *s)
+{
+	// Your code goes here
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * wildcmp - function that compares two strings and returns 1 if the strings can be considered identical, otherwise 0.
+ * @s2: contains the special character *.
+ * Return: 1 if the string is identical, otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	// Your code goes here
+}
