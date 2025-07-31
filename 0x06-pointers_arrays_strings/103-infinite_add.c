0d8b6918b6e5285b70c67d4e84d1f87506d7d51e
Dani-dzoe <danieldzotepe568@gmail.com>
2025-07-31 19:35:49 +0000

    main files added for checking the code and some other advanced tasks completed

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * infinite_add = adds two numbers
+ * @n1: number
+ * @n2: number
+ * @r: buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Returns: A pointer to the result, if result cannot be stored in r, function must return 0.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	// Your code goes here
+	
+}
